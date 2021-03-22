03e08be02ccf4ffa861b67318d5187d5a3d521dc
晓萌 杜 <dxm3dp@gmail.com>
2021-03-22 10:25:30 +0800

    整理 提交

---
+++ ShaderLearning/Assets/Shaders/Chapter11-Water.shader
@@ -0,0 +1,93 @@
+﻿Shader "Unlit/Chapter11-Water"
+{
+    Properties
+    {
+        // 主纹理
+        _MainTex ("Main Tex", 2D) = "white" {}
+        // 控制整体颜色
+        _Color ("Color Tint", Color) = (1, 1, 1, 1)
+        // 控制水流波动的幅度
+        _Magnitude ("Distortion Magnitude" , Float) = 1
+        // 控制波动的频率
+        _Frequency ("Distortion Frequency" , Float) = 1
+        // 控制波长的倒数
+        _InvWaveLength ("Distortion Inverse Wave Length", Float) = 10
+        // 
+        _Speed ("Speed", Float) = 0.1
+    }
+    SubShader
+    {
+        Tags {"Queue"="Transparent" "IgnoreProjector"="True" "RenderType"="Transparent" 
+            "DisableBatching"="True"}
+        //LOD 100
+
+        Pass
+        {
+            Tags {"LightMode"="ForwardBase"}
+            ZWrite Off
+            Blend SrcAlpha OneMinusSrcAlpha
+            Cull Off
+
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            // make fog work
+            //#pragma multi_compile_fog
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                //UNITY_FOG_COORDS(1)
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+            fixed4 _Color;
+            fixed _Magnitude;
+            fixed _Frequency;
+            float _InvWaveLength;
+            fixed _Speed;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                // 顶点的偏移量
+                float4 offset;
+                // 顶点的yzw分量不发生偏移
+                offset.yzw = float3(0.0, 0.0, 0.0);
+                // 计算顶点x分量的便宜
+                offset.x = sin(_Frequency * _Time.y + 
+                    v.vertex.x * _InvWaveLength + 
+                    v.vertex.y * _InvWaveLength + 
+                    v.vertex.z * _InvWaveLength ) * _Magnitude;
+                // 将偏移量添加到顶点坐标
+                o.vertex = UnityObjectToClipPos(v.vertex + offset);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                // 添加纹理动画
+                o.uv += float2( 0.0 , _Time.y * _Speed);
+                //UNITY_TRANSFER_FOG(o,o.vertex);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                // sample the texture
+                fixed4 col = tex2D(_MainTex, i.uv);
+                col.rgb *= _Color.rgb;
+                // apply fog
+                UNITY_APPLY_FOG(i.fogCoord, col);
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
---
+++ ShaderLearning/Assets/_Scripts/ProceduralTextureGeneration.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ProceduralTextureGeneration : MonoBehaviour
+{
+    public Material material = null;
+    private Texture2D generatedTexture = null;
+
+    #region Material properties
+
+    [SerializeField, SetProperty("TextureWidth")]
+    private int textureWidth = 512;
+    public int TextureWidth
+    {
+        get { return textureWidth; }
+        set
+        {
+            textureWidth = value;
+            UpdateMaterial();
+        }
+    }
+
+    [SerializeField, SetProperty("BackgroundColor")]
+    private Color backgroundColor = Color.white;
+    public Color BackgroundColor
+    {
+        get
+        {
+            return backgroundColor;
+        }
+        set
+        {
+            backgroundColor = value;
+            UpdateMaterial();
+        }
+    }
+
+    [SerializeField, SetProperty("CircleColor")]
+    private Color circleColor = Color.yellow;
+    public Color CircleColor
+    {
+        get
+        {
+            return circleColor;
+        }
+        set
+        {
+            circleColor = value;
+            UpdateMaterial();
+        }
+    }
+
+    private float blurFactor = 2.0f;
+    public float BlurFactor
+    {
+        get
+        {
+            return blurFactor;
+        }
+        set
+        {
+            blurFactor = value;
+            UpdateMaterial();
+        }
+    }
+    #endregion
+
+    void Start()
+    {
+        if (material == null)
+        {
+            Renderer renderer = gameObject.GetComponent<Renderer>();
+            if (renderer == null)
+            {
+                Debug.LogWarning("Cannor find a renderer.");
+                return;
+            }
+
+            material = renderer.sharedMaterial;
+        }
+
+        UpdateMaterial();
+    }
+
+    void UpdateMaterial()
+    {
+        if (material != null)
+        {
+            generatedTexture = GenerateProceduralTexture();
+            material.SetTexture("_MainTex", generatedTexture);
+        }
+    }
+
+    Texture2D GenerateProceduralTexture()
+    {
+        Texture2D proceduralTexture = new Texture2D(textureWidth, textureWidth);
+        // 定义圆与圆之间的间距
+        float circleInterval = textureWidth / 4f;
+        // 定义圆的半径
+        float radius = textureWidth / 10f;
+        // 定义模糊系数
+        float edgeBlur = 1f / blurFactor;
+
+        for (int w = 0; w < textureWidth; w++)
+        {
+            for (int h = 0; h < textureWidth; h++)
+            {
+                // 使用背景颜色进行初始化
+                Color pixel = backgroundColor;
+
+                // 
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        // 计算当前所绘制的圆的圆心位置
+                        Vector2 circleCenter = new Vector2(circleInterval * (i + 1), circleInterval * (j + 1));
+                        // 
+                        float dist = Vector2.Distance(new Vector2(w, h), circleCenter) - radius;
+                        // 
+                    }
+                }
+                proceduralTexture.SetPixel(w, h, color);
+            }
+        }
+        proceduralTexture.Apply();
+
+        return proceduralTexture;
+    }
+    Color color = Color.blue;
+}
